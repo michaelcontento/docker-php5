e8f068b9cf32f20eced9bc8ccf923ba595dcae7f
Michael Contento <michaelcontento@gmail.com>
2014-10-23 01:20:14 +0200

    remove (now) stupid case ...

--- run-php5.bash
+++ run-php5.bash
@@ -4,10 +4,6 @@ if [ $# -eq 0 ] || [ $1 == "serve" ]; then
     echo "===> Running php5-fpm"
     shift
     exec php5-fpm $@
-elif [ $1 == "composer" ]; then
-    echo "===> Running composer"
-    shift
-    exec composer $@
 else
     echo "===> Running command"
     exec $@
